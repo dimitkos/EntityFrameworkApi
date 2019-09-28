cbf579793dcbd14dfd35bb10ddc036431a4b5feb
dimitkos <33483947+dimitkos@users.noreply.github.com>
2019-09-29 01:51:12 +0300

    Add  Models

---
+++ Api.EntityTypes/Model/StudentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.EntityTypes.Model
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public AddressModel Address { get; set; }
+        public StandardModel Standard { get; set; }
+    }
+}
